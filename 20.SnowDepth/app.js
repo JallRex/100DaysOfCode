8642e8e2bbf3253605ffb9fee4093bc5017f242b
Jallrex <ludvigandre@gmail.com>
2022-11-29 15:34:27 +0100

    Base files for P20

---
+++ 20.SnowDepth/app.js
@@ -0,0 +1,24 @@
+//https://meowfacts.herokuapp.com/?count=3
+
+
+
+
+const url = "https://meowfacts.herokuapp.com/?count=10";
+
+
+
+
+
+//Get random number
+    function number(){
+        let number = Math.floor(Math.random()* url.length)
+        console.log(number)
+    }
+
+    number()
+
+
+
+
+
+    // https://htmldom.dev/loop-over-a-nodelist/
